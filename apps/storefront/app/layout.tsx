af742eed6761d27f9284b194cb9562a5dbf6769a
Jeffrey Segovia <jeff@showpo.com>
2024-05-14 15:49:47 +0800

    add page loading indicator

--- apps/storefront/app/layout.tsx
+++ apps/storefront/app/layout.tsx
@@ -1,15 +1,17 @@
-import Categories from '@/components/Categories';
-import { Footer } from '@/components/Footer';
-import { Header } from '@/components/Header';
-import HeaderWrapper from '@/components/HeaderWrapper';
-import { Providers } from '@/sdk/provider';
-import type { Metadata } from 'next';
-import { font } from './fonts/config';
-import './globals.css';
+import Categories from "@/components/Categories";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import HeaderWrapper from "@/components/HeaderWrapper";
+import { Providers } from "@/sdk/provider";
+import NextTopLoader from "nextjs-toploader";
+
+import type { Metadata } from "next";
+import { font } from "./fonts/config";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Showpo UK',
-  description: 'Showpo UK store',
+  title: "Showpo UK",
+  description: "Showpo UK store",
 };
 
 export default function RootLayout({
@@ -18,16 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${font.sans.variable} ${font.heading.variable} antialiased font-sans min-h-screen flex flex-col w-full`}
       >
+        <NextTopLoader color='#000' showSpinner={false} shadow={false} />
         <Providers>
           <HeaderWrapper>
             <Header />
             <Categories />
           </HeaderWrapper>
-          <main className="flex-1">{children}</main>
+          <main className='flex-1'>{children}</main>
           <Footer />
         </Providers>
       </body>
